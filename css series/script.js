dc92eeb9df88b098b7ea1cdec3c0e147ea3e79d2
Sandhiya06 <147073791+sandhiyaPM@users.noreply.github.com>
2025-01-06 12:27:55 +0530

    Delete css series directory

--- css series/script.js
+++
@@ -1,4 +0,0 @@
-function clearDisplay() { document.getElementById('display').innerText = ''; }
-function appendCharacter(character) { document.getElementById('display').innerText += character; }
-function calculateResult() { try { const display = document.getElementById('display'); display.innerText = eval(display.innerText.replace('^', '**')); } catch (e) { alert('Invalid expression'); } }
-function deleteLastCharacter() { const display = document.getElementById('display'); display.innerText = display.innerText.slice(0, -1); }
